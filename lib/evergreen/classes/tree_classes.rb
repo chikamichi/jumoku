1012e93b29eed72c210dff2cc5a93bddc37c7d64
Jean-Denis Vauguet <jd@vauguet.fr>
2010-03-30 19:00:09 +0200

    module typo

--- lib/evergreen/classes/tree_classes.rb
+++ lib/evergreen/classes/tree_classes.rb
@@ -1,4 +1,4 @@
-module Graphy
+module Evergreen
   # A generic {RawTreeBuilder RawTree} class you can inherit from.
   class RawTree;                 include RawTreeBuilder;                 end
 end
